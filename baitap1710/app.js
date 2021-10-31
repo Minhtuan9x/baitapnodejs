105298a0c52d75d9d9e9852cf32894d2f5a1f129
Minhtuan9x <0mtmtmt0@gmail.com>
2021-10-31 14:57:19 +0700

    up 31/10

--- baitap1710/app.js
+++ baitap1710/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const rou = require("./controllers/StudentControllers.js");
+const rou = require("./controllers/StudentController.js");
 var bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
